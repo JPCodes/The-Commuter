a6ff25c3d9fc67b92ab726bd6bc1e7376dc501d2
Jim Padilla <jp_code2013@yahoo.com>
2017-02-28 13:59:10 -0800

    Add Guardian Top Headlines API

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,12 @@
 class PagesController < ApplicationController
   include ApiHelper
   def index
-    if retrieve_home_articles then @articles = JSON.parse(retrieve_home_articles)['results'] else puts 'Articles not retrieved' end
+    if retrieve_guardian_headlines then @top_guardian_headlines = JSON.parse(retrieve_guardian_headlines) else puts 'Top Guardian Headlines not retrieved (PagesController#Index)' end
+
+    if retrieve_home_articles then @nyt_home_articles = JSON.parse(retrieve_home_articles)['results'] else puts 'NYT Home Articles not retrieved (PagesController#Index)' end
   end
 
   def congressional_report
-    @reports = JSON.parse(retrieve_congressional_bills)['results'][0]['bills']
+    if retrieve_congressional_bills then @reports = JSON.parse(retrieve_congressional_bills)['results'][0]['bills'] else puts 'Propublica Recent Bills Passed by Senate not retrieved (PagesController#Congressional_Report)' end
   end
 end
--- app/helpers/api_helper.rb
+++ app/helpers/api_helper.rb
@@ -10,6 +10,17 @@ module ApiHelper
     end
   end
 
+  def retrieve_guardian_headlines
+    begin
+      RestClient.get('http://content.guardianapis.com/search?page-size=15&q=us',
+      {
+        params: {'api-key': ENV['API_KEY_GUARDIAN']}
+      })
+    rescue
+      puts 'Guardian US Top Story Headlines API Error'
+    end
+  end
+
   def retrieve_congressional_bills
     begin
       RestClient.get("https://api.propublica.org/congress/v1/115/senate/bills/passed.json", headers={"X-API-Key": ENV['API_KEY_Propublica'] })
