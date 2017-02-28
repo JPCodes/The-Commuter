ad678bdbd30cfabf2fa34c16e2918393fb844df6
Jim Padilla <jp_code2013@yahoo.com>
2017-02-28 15:42:44 -0800

    Define routes, show_article controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,18 +4,30 @@ class PagesController < ApplicationController
     if retrieve_guardian_headlines then @top_guardian_headlines = JSON.parse(retrieve_guardian_headlines) else puts 'Top Guardian Headlines not retrieved (PagesController#Index)' end
 
     if retrieve_home_articles then @nyt_home_articles = JSON.parse(retrieve_home_articles)['results'] else puts 'NYT Home Articles not retrieved (PagesController#Index)' end
-
-    send_data(generate_pdf, :filename => "article.pdf", :type => "application/pdf")
   end
 
   def congressional_report
     if retrieve_congressional_bills then @reports = JSON.parse(retrieve_congressional_bills)['results'][0]['bills'] else puts 'Propublica Recent Bills Passed by Senate not retrieved (PagesController#Congressional_Report)' end
   end
 
+  def show_article
+    @article = params[:article]
+    if @article
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js
+      end
+    end
+  end
+
+  def download_pdf
+    send_data(generate_pdf, :filename => "article.pdf", :type => "application/pdf")
+  end
+
   private
   def generate_pdf
     Prawn::Document.new do
-        text "Hello Stackoverflow"
+        text "Hello World"
     end.render
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#index'
   resources :pages, :only => [:index]
   get '/congressional-report', to: 'pages#congressional_report'
+
+  get 'article', to: 'pages#show_article', as: 'show_article'
+  get 'download/article', to: 'pages#download_pdf', as: 'download_pdf'
 end
