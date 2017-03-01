f71fc1275b9ec1fa823d9625312d1165e1c6d5ee
Jim Padilla <jp_code2013@yahoo.com>
2017-02-28 17:50:31 -0800

    Add formatted prawn pdf

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   root to: 'pages#index'
   resources :pages, :only => [:index]
   get '/congressional-report', to: 'pages#congressional_report'
-
   get 'article', to: 'pages#show_article', as: 'show_article'
   get 'download/article', to: 'pages#download_pdf', as: 'download_pdf'
 end
