eb38eec9744c86eafecbca298e121eb3c67ddade
Jim Padilla <jp_code2013@yahoo.com>
2017-03-09 13:08:00 -0800

    Modify Production Settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,10 +1,11 @@
 Rails.application.configure do
   config.cache_classes = true
   config.eager_load = true
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  config.assets.js_compressor = :uglifier  config.assets.compile = false
+  config.assets.js_compressor = :uglifier
+  config.assets.compile = false
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
   config.log_level = :debug
