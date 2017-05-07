4f4d7b882bea953397fff63d1a226f45d8fd8fd7
Jim Padilla <jp_code2013@yahoo.com>
2017-05-07 15:16:10 -0700

    Implement static Google map for Yelp/Uber

--- app/controllers/ubers_controller.rb
+++ app/controllers/ubers_controller.rb
@@ -7,7 +7,7 @@ class UbersController < ApplicationController
       @end_longitude = params[:longitude]
       # @response = JSON.parse(getEstimatesForUserLocation(@end_latitude, @end_longitude))
       # @fastest_ride = @response['prices'].sort_by{ |hash| hash['duration'] }.first['duration']/60
-      # @map = "http://maps.googleapis.com/maps/api/staticmap?zoom=17&format=png&sensor=false&size=280x280&maptype=roadmap&style=element:geometry.fill|color:0xf4f4f4&markers=color:red|" + @end_latitude + "," + @end_longitude + "&scale=2&key=" + ENV['GOOGLE_API_KEY']
+      @map = "http://maps.googleapis.com/maps/api/staticmap?zoom=17&format=png&sensor=false&size=280x280&maptype=roadmap&style=element:geometry.fill|color:0xf4f4f4&markers=color:red|" + @end_latitude + "," + @end_longitude + "&scale=2&key=" + ENV['GOOGLE_API_KEY']
     end
     # @get_map = get_google_map
   end
