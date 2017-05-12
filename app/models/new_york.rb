27fc0ce4109396c7a58a496cdde1fedaa76aac72
Jim Padilla <jp_code2013@yahoo.com>
2017-05-11 22:43:18 -0700

    Add test case for API data caching

--- app/models/new_york.rb
+++ app/models/new_york.rb
@@ -1,6 +1,7 @@
 class NewYork < ActiveRecord::Base
   has_one :news_cache, as: :content_storage
   validates :update_interval, :content_type, presence: true
+  before_save :cache_api_data
 
   enum content_type: {
     home: 0,
@@ -8,4 +9,9 @@ class NewYork < ActiveRecord::Base
     tech: 2,
     movies: 3
   }
+
+  def cache_api_data
+    # get api results
+    content_storage = NewsCache.new(content_storage_body: 'testing')
+  end
 end
