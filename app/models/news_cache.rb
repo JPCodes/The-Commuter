e3a1751fcd61cfbd0074a9cd38f72a44ce50faae
Jim Padilla <jp_code2013@yahoo.com>
2017-05-12 23:43:12 -0700

    Add methods to figure out which content provider's stash should be called

--- app/models/news_cache.rb
+++ app/models/news_cache.rb
@@ -1,3 +1,23 @@
 class NewsCache < ActiveRecord::Base
   belongs_to :content_storage, polymorphic: true
+
+  def self.cached_model_enums
+    [['NewYork', NewYork.content_types], ['NewsGuardian', NewsGuardian.content_types]]
+  end
+
+  def self.find_specified_content(preference_enum)
+    NewsCache.cached_model_enums.each do |model_enum|
+      enum_hash = model_enum[1]
+      if enum_hash.has_value? preference_enum
+        model_name = model_enum[0]
+        content_name = model_enum[1].key(preference_enum)
+        return retrieve_cached_content(model_name, content_name)
+      end
+    end
+  end
+
+  def self.retrieve_cached_content(model_name, content_name)
+    puts model_name
+    puts content_name
+  end
 end
