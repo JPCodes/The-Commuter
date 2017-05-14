0ecc07794879de764692ebcf448e240ce9acc116
Jim Padilla <jp_code2013@yahoo.com>
2017-05-13 19:28:08 -0700

    Rework dashboard content sections, write methods to return 1 day api backup

--- app/helpers/dashboards_helper.rb
+++ app/helpers/dashboards_helper.rb
@@ -1,5 +1,6 @@
 module DashboardsHelper
   def generate_dashboard_content(preference_enum)
-    NewsCache.find_specified_content(preference_enum)
+    result = NewsCache.find_specified_content(preference_enum)
+    result
   end
 end
